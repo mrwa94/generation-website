3636d0fe9ec325801830649a2754aa78c2e3bc44
mrwa94 <90597166+mrwa94@users.noreply.github.com>
2022-03-29 21:48:29 +0300

    navbar and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <div>
+
+<footer className=' footer  ' >
+
+<p>2021 © REgeneration Energy  <br/>
+All rights reserved
+</p>
+
+<div className='d-grid gap-3' >
+<a src = ''  href="#"> Home </a>
+<a src = '' href="#"> About us</a>
+<a src = ''href="#" > contact</a>
+</div>
+
+<div className='d-grid gap-3' >
+<a src = ''  href="#"> Twitter </a>
+<a src = ''href="#" > Linkedin</a>
+</div>
+
+<div className='d-grid gap-3' >
+<a src = '' href="#" > Privacy Policy </a>
+<a src = ''  href="#"> Terms Of service</a>
+</div>
+
+
+
+</footer>
+
+    </div>
+  )
+}
+
+export default Footer
